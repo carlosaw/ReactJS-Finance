34d49eb6c06898654d00c3b619c885f3c1e2d37b
carlosaw <carlosfreevg@gmail.com>
2023-09-21 16:19:58 -0400

    expense

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,23 @@ import { Category } from './types/Category';
 import { categories } from './data/categories';
 import { items } from './data/items';
 import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter';
+import { TableArea } from './components/TableArea';
+import { InfoArea } from './components/InfoArea';
 
 const App = () => {
   const [list, setList] = useState(items);
   const [filteredList, setFilteredList] = useState<Item[]>([]);
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
 
   useEffect(() => {
     setFilteredList( filterListByMonth(list, currentMonth) );
   }, [list, currentMonth]);
+
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth);
+  }
 
   return (
     <C.Container>
@@ -22,8 +30,17 @@ const App = () => {
       </C.Header>
       <C.Body>
         {/* Área de informações */}
+        <InfoArea 
+          currentMonth={currentMonth}
+          onMonthChange={handleMonthChange}
+          income={income}
+          expense={expense}
+        />
+
         {/* Área de inserções */}
+
         {/* Tabela de itens */}
+        <TableArea list={filteredList} />
       </C.Body>
     </C.Container>
   );
--- src/data/items.ts
+++ src/data/items.ts
@@ -1,8 +1,8 @@
 import { Item } from "../types/Item";
 
 export const items: Item[] = [
-  { date: new Date(2021, 9, 15), category: 'food', title: 'MacDonalds', value: 32.12 },
-  { date: new Date(2021, 9, 15), category: 'food', title: 'BurgerKing', value: 28 },
-  { date: new Date(2021, 9, 16), category: 'rent', title: 'Aluguel Apto', value: 2300 },
-  { date: new Date(2021, 9, 18), category: 'salary', title: 'AWRegulagens', value: 4500 }
+  { date: new Date(2023, 9, 6), category: 'food', title: 'MacDonalds', value: 32.12 },
+  { date: new Date(2023, 9, 15), category: 'food', title: 'BurgerKing', value: 28 },
+  { date: new Date(2023, 9, 16), category: 'rent', title: 'Aluguel Apto', value: 2300 },
+  { date: new Date(2023, 10, 18), category: 'salary', title: 'AWRegulagens', value: 4500 }
 ];
